326f36ac32933ec97de63fce6b7d24edfece1e3c
JacobzYan <70812157+JacobzYan@users.noreply.github.com>
2020-11-14 15:35:44 -0600

    Updated functions, variables
    Added values in lieu of placeholders to variables and calculated needed advantages
    rethought maximum pedal displacement method

--- BrakingForceTorqueRelations.py
+++ BrakingForceTorqueRelations.py
@@ -7,51 +7,54 @@
 # ALL MUST BE IN SI UNITS (meters, kgNewtons, newtons)
 
 # Determined:
-pedal_mechanical_advantage = 5
-hydraulic_advantage = 40
+pedal_mechanical_advantage = 1.80841602268  # Calculated with this program
+hydraulic_advantage = 21.2064
+# Surface area of master cylinder = 1.88^2 * pi in^2, slave cylinder SA= pi in^2, ratio =1.88^2, feeds into 6 slave
+# pistons(3 caliper assem. 2 pistons each), =(1.88/1)^2 * 6, see trade study parts
 
 # Measured:
-brake_rotor_radius = 1
-coef_friction = 0.8
-real_pedal_force = 10
-brake_torque = 1000
-number_of_brakes = 2
-max_pedal_displacement = 0.5
+brake_rotor_radius = 0.1016 # see trade study parts
+coef_friction = 0.275  # See trade study, brake pad paper should verify with our equipment
+real_pedal_force = 400  # See trade study pedal force paper
+brake_torque = 428.59952784  # see DecelerationCalculations.py in gitub
+number_of_brakes = 3
+piston_stroke = 0.02794  # See trade study parts
 
 
 # Calculations start here
 
 
 def calculated_pedal_force():
-    return number_of_brakes * brake_torque / brake_rotor_radius / hydraulic_advantage / pedal_mechanical_advantage
+    return brake_torque / brake_rotor_radius / hydraulic_advantage / pedal_mechanical_advantage / coef_friction
 
 
 # equals the product of the needed mechanical and hydraulic advantages
 def calculated_total_advantage():
-    return brake_torque * number_of_brakes / brake_rotor_radius / real_pedal_force
+    return brake_torque / brake_rotor_radius / real_pedal_force / coef_friction
 
 
 def calculated_brake_torque():
-    return real_pedal_force * pedal_mechanical_advantage * hydraulic_advantage * brake_rotor_radius / number_of_brakes
+    return real_pedal_force * pedal_mechanical_advantage * hydraulic_advantage * brake_rotor_radius * coef_friction
 
 
-def max_caliper_displacement():
-    return max_pedal_displacement / pedal_mechanical_advantage / hydraulic_advantage
-
+def pedal_displacement():
+    return piston_stroke * pedal_mechanical_advantage
 
 # Display
+
+
 print("CALCULATED AND GIVEN PARAMETERS MUST MATCH\nAdjust mechanical and hydraulic advantages to correspond with the "
       "calculated total advantage\n")
 print("Given parameters:")
 print("Pedal force: ", real_pedal_force, "N")
 print("Total advantage: ", str(pedal_mechanical_advantage * hydraulic_advantage))
-print("Brake torque: ", brake_torque, " Nm")
+print("Brake torque : ", brake_torque, " Nm")
 
 
-print("\nCalculated values: ")
-print("Pedal force: ", calculated_pedal_force(), "N")
-print("Total advantage: ", calculated_total_advantage())
-print("Brake torque: ", calculated_brake_torque(), " Nm")
+print("\nCalculated values(should match given parameters): ")
+print("Pedal force needed: ", calculated_pedal_force(), "N")
+print("Total advantage needed: ", calculated_total_advantage())
+print("Brake torque needed: ", calculated_brake_torque(), " Nm")
 
 
-print("\nMax caliper displacement: ", max_caliper_displacement(), " M")
+print("\nMin pedal displacement: ", pedal_displacement(), " M")
